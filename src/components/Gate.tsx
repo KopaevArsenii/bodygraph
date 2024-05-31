28914fbc500ab649af3241c276787666403c29f8
ArseniiKopaev <senya.kopaev@mail.ru>
2024-05-31 23:53:08 +0300

    made animations for gates

--- src/components/Gate.tsx
+++ src/components/Gate.tsx
@@ -8,7 +8,17 @@ interface GateProps {
 
 const Gate: FC<GateProps> = ({ active, gate }) => {
   const { value, position } = gate;
-  return <text x={position.x} y={position.y} textAnchor="middle" fill={active ? "white" : "black"}>{value}</text>;
+  return (
+    <text x={position.x} y={position.y} textAnchor="middle" fill={active ? "white" : "black"}>
+      <animate
+        attributeName="opacity"
+        values="0;1"
+        dur="0.5s"
+        fill="freeze"
+      />
+      {value}
+    </text>
+  );
 }
 
 export default Gate;
