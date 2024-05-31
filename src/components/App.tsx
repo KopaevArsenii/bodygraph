63c296714bd40c77a300e6f1094b0bdd513e2e45
ArseniiKopaev <senya.kopaev@mail.ru>
2024-05-31 23:19:14 +0300

    refactoring

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,33 @@
-import './App.css'
-import Diagram from './Diagram.tsx';
+import React, { useState } from 'react';
+import Figure from './Figure.tsx';
+import { figures, lines, points } from '../constants/figures.ts';
+import Line from './Line.tsx';
+import Gate from './Gate.tsx';
 
-function App() {
+const initialSetValues = ["62", "24", "1", "2", "26", "44", "34", "32", "28", "27", "60", "49", "41"];
 
-  return <Diagram />;
-}
+const App: React.FC = () => {
+  const [, setCounter] = useState<number>(0);
+  const updateUi = () => setCounter((prev) => prev + 1);
 
-export default App
+  const [selectedPoints] = useState<Set<string>>(new Set<string>(initialSetValues));
+
+  const handleMouseMove = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    const { clientX, clientY } = event;
+    console.log(`Mouse Coordinates: X: ${clientX}, Y: ${clientY}`);
+  };
+
+  return (
+    <svg width="800" height="800" onMouseMove={handleMouseMove}>
+      {lines.map((line) => {
+        const formPoint = points.find((point) => point.value === line.form);
+        const toPoint = points.find((point) => point.value === line.to);
+        return  formPoint && toPoint ? <Line from={formPoint} to={toPoint} /> : null;
+      })}
+      {figures.map((figure) => <Figure points={figure} />)}
+      {points.map((point) => <Gate active={selectedPoints.has(point.value)} gate={point} />)}
+    </svg>
+  );
+};
+
+export default App;
--- src/components/Gate.tsx
+++ src/components/Gate.tsx
@@ -2,11 +2,12 @@ import { FC } from 'react';
 import { Point } from '../types';
 
 interface GateProps {
+  active: boolean,
   gate: Point,
 }
 
-const Gate: FC<GateProps> = ({ gate }) => {
-  const { active, value, position } = gate;
+const Gate: FC<GateProps> = ({ active, gate }) => {
+  const { value, position } = gate;
   return <text x={position.x} y={position.y} textAnchor="middle" fill={active ? "white" : "black"}>{value}</text>;
 }
 
